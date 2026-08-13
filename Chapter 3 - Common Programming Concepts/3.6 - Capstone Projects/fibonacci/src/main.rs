b78238c149139deeaf49292b57606a6d1dbf44bf
Edward Simpson-Fitzgibbon <edward.psf@gmail.com>
2026-08-13 08:38:05 -0700

    Added first solution for Fibonacci Sequence

--- Chapter 3 - Common Programming Concepts/3.6 - Capstone Projects/fibonacci/src/main.rs
+++ Chapter 3 - Common Programming Concepts/3.6 - Capstone Projects/fibonacci/src/main.rs
@@ -1,3 +1,30 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut first = 0;
+    let mut second = 1;
+
+    println!("Enter a number to get the nth number in the Fibonacci Sequence:");
+
+    let mut n = String::new();
+    std::io::stdin()
+        .read_line(&mut n)
+        .expect("Failed to read line");
+
+    let n: u32 = match n.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("Please enter a valid number.");
+            return;
+        }
+    };
+
+    let mut current = first;
+    for _ in 0..n {
+        current = first + second;
+        first = second;
+        second = current;
+    }
+
+    println!("The {}th number in the Fibonacci Sequence is: {}", n, first);
 }
