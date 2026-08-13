dd044416b78079c0992685c1a415b36a0fcbdfce
Edward Simpson-Fitzgibbon <edward.psf@gmail.com>
2026-08-13 10:16:20 -0700

    Initial attempt at xmas_carol

--- Chapter 3 - Common Programming Concepts/3.6 - Capstone Projects/xmas_carol/src/main.rs
+++ Chapter 3 - Common Programming Concepts/3.6 - Capstone Projects/xmas_carol/src/main.rs
@@ -1,3 +1,47 @@
 fn main() {
-    println!("Hello, world!");
+    let gifts: [&str; 12] = [
+        "A partridge in a pear tree",
+        "Two turtle doves",
+        "Three French hens",
+        "Four calling birds",
+        "Five golden rings",
+        "Six geese a-laying",
+        "Seven swans a-swimming",
+        "Eight maids a-milking",
+        "Nine ladies dancing",
+        "Ten lords a-leaping",
+        "Eleven pipers piping",
+        "Twelve drummers drumming"
+    ]
+
+    let mut curr_verse: u8 = 0;
+
+    while curr_verse < 12 {
+        let day = match curr_verse {
+            0 => "first",
+            1 => "second",
+            2 => "third",
+            3 => "fourth",
+            4 => "fifth",
+            5 => "sixth",
+            6 => "seventh",
+            7 => "eighth",
+            8 => "ninth",
+            9 => "tenth",
+            10 => "eleventh",
+            11 => "twelfth",
+            _ => unreachable!(),
+        };
+
+        println!("\nOn the {} day of Christmas", day);
+        println!("My true love gave to me");
+
+        for i in (0..=curr_verse).rev() {
+            if i == 0 && curr_verse > 0 {
+                println!("And {}", gifts[i]);
+            } else {
+                println!("{}", gifts[i]);
+            }
+        }
+    }
 }
