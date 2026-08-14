29180c750a26cb8dc7bdab086c921b85a8d00dbc
Edward Simpson-Fitzgibbon <edward.psf@gmail.com>
2026-08-13 23:04:36 -0700

    Added Chapter 4: References

---
+++ Chapter 4 - Understanding Ownership/Chapter 4.2 - References and Borrowing/references/src/main.rs
@@ -0,0 +1,23 @@
+fn main() {
+    let s1 = String::from("hello");
+
+    let len = calculate_length(&s1);
+
+    println!("The length of '{}' is {}.", s1, len);
+
+    let mut s2 = String::from("hello");
+
+    println!("Before change: {}", s2);
+
+    change(&mut s2);
+
+    println!("After change: {}", s2);
+}
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
+}
